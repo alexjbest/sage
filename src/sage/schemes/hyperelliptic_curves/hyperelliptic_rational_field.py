7b4e96f5d627cbb6deb2a48b4340b8d7ae153391
Alex J Best <alex.j.best@gmail.com>
2023-02-08 21:41:25 +0000

    add minimal discriminant

--- src/sage/schemes/hyperelliptic_curves/hyperelliptic_rational_field.py
+++ src/sage/schemes/hyperelliptic_curves/hyperelliptic_rational_field.py
@@ -12,6 +12,8 @@ import sage.rings.abc
 from sage.rings.padics.all import pAdicField
 
 from sage.schemes.curves.projective_curve import ProjectivePlaneCurve_field
+from sage.libs.pari.all import pari
+from sage.misc.cachefunc import cached_method
 
 from . import hyperelliptic_generic
 
@@ -36,6 +38,58 @@ class HyperellipticCurve_rational_field(hyperelliptic_generic.HyperellipticCurve
             K = pAdicField(p, prec)
         frob_p, forms = monsky_washnitzer.matrix_of_frobenius_hyperelliptic(my_chage_ring(self, K))
         return frob_p
+
+    @cached_method
+    def minimal_discriminant(self):
+        r"""
+        Return the minimal minimal_discriminant of this hyperelliptic curve.
+
+        This method is cached.
+
+        EXAMPLES::
+
+            sage: F,G = EllipticCurve([0,0,1,-1,0]).hyperelliptic_polynomials()
+            sage: E = HyperellipticCurve(F,G)
+            sage: E.minimal_discriminant()
+            37
+
+            sage: F,G = EllipticCurve([0, -1, 1, -10, -20]).hyperelliptic_polynomials()
+            sage: E = HyperellipticCurve(F,G)
+            sage: E.minimal_discriminant()
+            -161051
+
+        LMFDB curve 113236.a.452944.1::
+
+            sage: R = PolynomialRing(QQ, "x")
+            sage: C = HyperellipticCurve(R([0, 2, 12, 24, 16, 1]), R([0, 1, 1]))
+            sage: C.minimal_discriminant()
+            -452944
+            sage: X = HyperellipticCurve(R([0, 8, 49, 98, 65, 4]))
+            sage: X.minimal_discriminant()
+            -452944
+
+        LMFDB curve 3125.a.3125.1::
+
+            sage: R = PolynomialRing(QQ, "x")
+            sage: C = HyperellipticCurve(R([256, 0, 0, 0, 0, 1]), R([]))
+            sage: C.minimal_discriminant()
+            3125
+            sage: X = HyperellipticCurve(R([0,0,0,0,0,1]), R(1))
+            sage: X.minimal_discriminant()
+            3125
+
+        LMFDB curve 400.a.409600.1::
+
+            sage: R = PolynomialRing(QQ, "x")
+            sage: C = HyperellipticCurve(R([1, 0, 4, 0, 4, 0, 1]), R([]))
+            sage: C.minimal_discriminant()
+            -409600
+            sage: X = HyperellipticCurve(R([1, 0, 4, 0, 4, 0, 1]))
+            sage: X.minimal_discriminant()
+            -409600
+
+        """
+        return pari(self.hyperelliptic_polynomials()).hyperellminimaldisc()
 
     def lseries(self, prec=53):
         """
